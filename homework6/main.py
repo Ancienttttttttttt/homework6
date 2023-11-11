22bc34de50db9b0dc57b7f2e125e91768b4e5457
Ancienttttttttttt <pila33rus_RU@mail.ru>
2023-11-11 21:44:41 +0300

    1234

---
+++ homework6/main.py
@@ -0,0 +1,15 @@
+#Zadacha 8
+# a = [0, 1, 2, 3, 3, 4, 4]
+# a = list(set(a))
+# print(a)
+
+#Zadacha 9
+# a = [1, 0, 5, 4, 14, 2, 44, 6, 7, 88, 45, 33]
+# a.sort()
+# print(a)
+
+#Zadacha 10
+# list_a = [0, 1, 2, 3, 4, 5]
+# list_b = [6, 7, 8, 9, 10]
+# list_c = list_a + list_b
+# print(list_c)
